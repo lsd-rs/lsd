4b9568510c0a771fa5566bb09217f6efc5562f2d
Peltoche <dev@halium.fr>
2018-11-16 14:19:07 +0100

    Change a lot of stuff
    This is a shitty comment, I know...

---
+++ src/core.rs
@@ -0,0 +1,206 @@
+use formatter::*;
+use size::*;
+use std::cmp::Ordering;
+use std::fs::{read_link, Metadata};
+use std::os::unix::fs::MetadataExt;
+use std::path::{Path, PathBuf};
+use users::{get_group_by_gid, get_user_by_uid};
+use Options;
+
+pub struct Core<'a> {
+    formatter: Formatter,
+    options: &'a Options,
+}
+
+#[derive(Debug)]
+pub struct Meta {
+    pub path: PathBuf,
+    pub metadata: Metadata,
+    pub group: String,
+    pub user: String,
+    pub symlink: Option<String>,
+    pub size_value: String,
+    pub size_unit: String,
+}
+
+impl<'a> Core<'a> {
+    pub fn new(options: &'a Options) -> Core<'a> {
+        Core {
+            options: options,
+            formatter: Formatter::new(),
+        }
+    }
+
+    pub fn print(&self, inputs: Vec<&str>) {
+        let print_folder_name: bool = inputs.len() > 1;
+
+        let mut dirs = Vec::new();
+        let mut files = Vec::new();
+
+        for input in inputs {
+            let path = Path::new(input);
+
+            if path.is_dir() {
+                dirs.push(path);
+            } else {
+                files.push(self.path_to_meta(&path).unwrap());
+            }
+        }
+
+        files.sort_unstable_by(sort_by_meta);
+        dirs.sort_unstable();
+
+        if files.len() > 0 {
+            self.print_long(&mut files);
+        }
+
+        for dir in dirs {
+            let folder_metas = self.list_folder_content(dir);
+            if print_folder_name {
+                println!("\n{}:", dir.display())
+            }
+            self.print_long(&folder_metas);
+        }
+    }
+
+    pub fn list_folder_content(&self, folder: &Path) -> Vec<Meta> {
+        let mut content: Vec<Meta> = Vec::new();
+
+        for entry in folder.read_dir().expect("read_dir call failed") {
+            if let Ok(entry) = entry {
+                if let Some(meta) = self.path_to_meta(entry.path().as_path()) {
+                    content.push(meta);
+                }
+            }
+        }
+
+        content.sort_unstable_by(sort_by_meta);
+
+        content
+    }
+
+    pub fn path_to_meta(&self, path: &Path) -> Option<Meta> {
+        let mut name: Option<&str> = None;
+        if let Some(os_str_name) = path.file_name() {
+            if let Some(name_str) = os_str_name.to_str() {
+                name = Some(name_str);
+            }
+        }
+
+        if name.is_none() {
+            println!("failed to retrieve file name for {}", path.display());
+            return None;
+        }
+
+        // Skip the hidden files if the 'display_all' option is not set.
+        if name.unwrap().starts_with(".") && !self.options.display_all {
+            return None;
+        }
+
+        let meta;
+        let mut symlink = None;
+        if let Ok(res) = read_link(path) {
+            meta = path.symlink_metadata().unwrap();
+            symlink = Some(res.to_str().unwrap().to_string());
+        } else {
+            meta = path.metadata().unwrap();
+        }
+
+        let user = get_user_by_uid(meta.uid())
+            .unwrap()
+            .name()
+            .to_str()
+            .unwrap()
+            .to_string();
+
+        let group = get_group_by_gid(meta.gid())
+            .unwrap()
+            .name()
+            .to_str()
+            .unwrap()
+            .to_string();
+
+        let size = Size::Bytes(meta.len()).to_string(Base::Base10, Style::Abbreviated);
+        let size_parts: Vec<&str> = size.split(' ').collect();
+
+        Some(Meta {
+            path: path.to_path_buf(),
+            metadata: meta,
+            user: user,
+            group: group,
+            symlink: symlink,
+            size_value: size_parts[0].to_string(),
+            size_unit: size_parts[1].to_string(),
+        })
+    }
+
+    fn print_long(&self, metas: &Vec<Meta>) {
+        let max_user_length = self.detect_user_lenght(metas);
+        let max_group_length = self.detect_group_lenght(metas);
+        let (max_size_value_length, max_size_unit_length) = self.detect_size_lenghts(metas);
+
+        for meta in metas {
+            print!(
+                "  {}  {}  {}  {}  {}  {}\n",
+                self.formatter.format_permissions(&meta),
+                self.formatter.format_user(&meta.user, max_user_length),
+                self.formatter.format_group(&meta.group, max_group_length),
+                self.formatter
+                    .format_size(&meta, max_size_value_length, max_size_unit_length),
+                self.formatter.format_date(&meta),
+                self.formatter.format_name(&meta),
+            );
+        }
+    }
+
+    fn detect_user_lenght(&self, paths: &Vec<Meta>) -> usize {
+        let mut max: usize = 0;
+
+        for path in paths {
+            if path.user.len() > max {
+                max = path.user.len();
+            }
+        }
+
+        max
+    }
+
+    fn detect_group_lenght(&self, paths: &Vec<Meta>) -> usize {
+        let mut max: usize = 0;
+
+        for path in paths {
+            if path.group.len() > max {
+                max = path.group.len();
+            }
+        }
+
+        max
+    }
+
+    fn detect_size_lenghts(&self, paths: &Vec<Meta>) -> (usize, usize) {
+        let mut max_value_length: usize = 0;
+        let mut max_unit_size: usize = 0;
+
+        for path in paths {
+            if path.size_value.len() > max_value_length {
+                max_value_length = path.size_value.len();
+            }
+
+            if path.size_unit.len() > max_unit_size {
+                max_unit_size = path.size_unit.len();
+            }
+        }
+
+        (max_value_length, max_unit_size)
+    }
+}
+
+fn sort_by_meta(a: &Meta, b: &Meta) -> Ordering {
+    if a.path.is_dir() == b.path.is_dir() {
+        a.path.cmp(&b.path)
+    } else if a.path.is_dir() && b.path.is_file() {
+        Ordering::Less
+    } else {
+        Ordering::Greater
+    }
+}
--- src/formatter.rs
+++ src/formatter.rs
@@ -1,5 +1,5 @@
 use ansi_term::Colour;
-use path_lister::Path;
+use core::Meta;
 use std::collections::HashMap;
 use std::os::unix::fs::PermissionsExt;
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
@@ -8,70 +8,72 @@ use time::Timespec;
 const HOURE: u64 = 3600; // 1 HOURE == 3600 seconds
 const DAY: u64 = HOURE * 24; // 1 DAY == 25 HOURE
 
-#[derive(Hash, Debug, Eq, PartialEq, Copy, Clone)]
-pub enum PathKind {
-    UnrecognizedFile,
-    RecognizedFile,
-    Dir,
-}
-
-lazy_static! {
-    pub static ref PathKindColor: HashMap<PathKind, (u8, u8, u8)> = {
-        let mut m = HashMap::new();
-
-        m.insert(PathKind::UnrecognizedFile, (0xFF, 0xFF, 0x04)); // gold
-        m.insert(PathKind::RecognizedFile, (0x04, 0xFF, 0x04)); // limon
-        m.insert(PathKind::Dir, (0x00, 0xAF, 0xFF)); // dodgerblue
-
-        m
-    };
-}
-
-#[derive(Hash, Debug, Eq, PartialEq, Copy, Clone)]
-pub enum LastTimeModified {
-    DayOld,
-    HourOld,
-    Older,
-}
-
-lazy_static! {
-    pub static ref LastTimeModifiedColor: HashMap<LastTimeModified, (u8, u8, u8)> = {
-        let mut m = HashMap::new();
-
-        m.insert(LastTimeModified::HourOld, (0x2C, 0xFF, 0x2C));
-        m.insert(LastTimeModified::DayOld, (0x1C, 0xFF, 0xB7));
-        m.insert(LastTimeModified::Older, (0x63, 0xB1, 0x8A));
-
-        m
-    };
-}
-
 #[allow(dead_code)]
 #[derive(Hash, Debug, Eq, PartialEq, Copy, Clone)]
 pub enum Elem {
+    /// Path Kind
+    UnrecognizedFile,
+    RecognizedFile,
+    Dir,
+
+    /// Permissions
+    Read,
+    Write,
+    Exec,
+    NoAccess,
+
+    /// Last Time Modified
+    DayOld,
+    HourOld,
+    Older,
+
     /// Link
     DeadLink,
     Link,
+
+    /// User / Group Name
+    User,
+    Group,
 
     /// File Size
     FileLarge,
     FileMedium,
     FileSmall,
+}
 
-    /// Random
-    Report,
-    User,
-    Tree,
-    Empty,
-    Error,
-    Normal,
+lazy_static! {
+    pub static ref Colors: HashMap<Elem, Colour> = {
+        let mut m = HashMap::new();
+        // User / Group
+        m.insert(Elem::User, Colour::RGB(0xFF, 0xFF, 0xD8));
+        m.insert(Elem::Group, Colour::RGB(0xD9, 0xD9, 0x8F));
 
-    /// Git
-    Addition,
-    Modification,
-    Deletion,
-    Untracked,
-    Unchanged,
+        // Permissions
+        m.insert(Elem::Read, Colour::RGB(0x5f, 0xD7, 0x5F));
+        m.insert(Elem::Write, Colour::RGB(0xD7, 0xD7, 0x87));
+        m.insert(Elem::Exec, Colour::RGB(0xCD, 0x3A, 0x3A));
+        m.insert(Elem::NoAccess, Colour::RGB(0xD7, 0x89, 0x89));
+
+        // Path Kind
+        m.insert(Elem::UnrecognizedFile, Colour::RGB(0xFF, 0xFF, 0x04)); // gold
+        m.insert(Elem::RecognizedFile, Colour::RGB(0x04, 0xFF, 0x04)); // limon
+        m.insert(Elem::Dir, Colour::RGB(0x00, 0xAF, 0xFF)); // dodgerblue
+
+        // Last Time Modified
+        m.insert(Elem::HourOld, Colour::RGB(0x2C, 0xFF, 0x2C));
+        m.insert(Elem::DayOld, Colour::RGB(0x1C, 0xFF, 0xB7));
+        m.insert(Elem::Older, Colour::RGB(0x63, 0xB1, 0x8A));
+
+        // Last Time Modified
+        m.insert(Elem::FileSmall, Colour::RGB(0xFF, 0xFF, 0xD9));
+        m.insert(Elem::FileMedium, Colour::RGB(0x1C, 0xFF, 0xB7));
+        m.insert(Elem::FileLarge, Colour::RGB(0xFF, 0xB0, 0x00));
+
+        // Link
+        m.insert(Elem::Link, Colour::RGB(0x3B, 0xCE, 0xCe));
+
+        m
+    };
 }
 
 pub struct Formatter {}
@@ -81,29 +83,38 @@ impl Formatter {
         Formatter {}
     }
 
-    pub fn format_path(&self, content: &str, path: &Path) -> String {
-        let color = match path.metadata.is_dir() {
-            true => PathKindColor[&PathKind::Dir],
-            false => PathKindColor[&PathKind::UnrecognizedFile],
+    pub fn format_name(&self, meta: &Meta) -> String {
+        let mut content = String::new();
+
+        let color = match meta.metadata.is_dir() {
+            true => Colors[&Elem::Dir],
+            false => Colors[&Elem::UnrecognizedFile],
         };
 
-        Colour::RGB(color.0, color.1, color.2)
-            .paint(content)
-            .to_string()
+        let file_name = meta.path.file_name().unwrap().to_str().unwrap();
+        content = content + &color.paint(file_name).to_string();
+
+        let color = Colors[&Elem::Link];
+        if let Some(ref link) = meta.symlink {
+            content =
+                content + &color.paint(String::from(" ⇒ ") + &color.paint(link).to_string());
+        }
+
+        content
     }
 
-    pub fn format_date(&self, path: &Path) -> String {
-        let modified_time = path.metadata.modified().unwrap();
+    pub fn format_date(&self, meta: &Meta) -> String {
+        let modified_time = meta.metadata.modified().unwrap();
 
         let now = SystemTime::now();
 
         let color;
         if modified_time > now - Duration::new(HOURE, 0) {
-            color = LastTimeModifiedColor[&LastTimeModified::HourOld];
+            color = Colors[&Elem::HourOld];
         } else if modified_time > now - Duration::new(DAY, 0) {
-            color = LastTimeModifiedColor[&LastTimeModified::DayOld];
+            color = Colors[&Elem::DayOld];
         } else {
-            color = LastTimeModifiedColor[&LastTimeModified::Older];
+            color = Colors[&Elem::Older];
         }
 
         let modified_time_since_epoch = modified_time.duration_since(UNIX_EPOCH).unwrap();
@@ -112,28 +123,18 @@ impl Formatter {
             modified_time_since_epoch.subsec_nanos() as i32,
         ));
 
-        Colour::RGB(color.0, color.1, color.2)
-            .paint(time.ctime().to_string())
-            .to_string()
+        color.paint(time.ctime().to_string()).to_string()
     }
 
-    pub fn format_permissions(&self, path: &Path) -> String {
+    pub fn format_permissions(&self, meta: &Meta) -> String {
         let mut res = String::with_capacity(10);
 
-        let mode = path.metadata.permissions().mode();
+        let mode = meta.metadata.permissions().mode();
 
-        let read_perm = Colour::RGB(0x5f, 0xD7, 0x5F)
-            .paint(String::from("r"))
-            .to_string();
-        let write_perm = Colour::RGB(0xD7, 0xD7, 0x87)
-            .paint(String::from("w"))
-            .to_string();
-        let exec_perm = Colour::RGB(0xCD, 0x3A, 0x3A)
-            .paint(String::from("x"))
-            .to_string();
-        let no_access = Colour::RGB(0xD7, 0x89, 0x89)
-            .paint(String::from("-"))
-            .to_string();
+        let read_perm = Colors[&Elem::Read].paint(String::from("r")).to_string();
+        let write_perm = Colors[&Elem::Write].paint(String::from("w")).to_string();
+        let exec_perm = Colors[&Elem::Exec].paint(String::from("x")).to_string();
+        let no_access = Colors[&Elem::NoAccess].paint(String::from("-")).to_string();
 
         // User Read Permisssions
         match mode & 0o400 {
@@ -190,5 +191,65 @@ impl Formatter {
         }
 
         res.to_string()
+    }
+
+    pub fn format_user(&self, user_name: &String, max_user_size: usize) -> String {
+        if user_name.len() == max_user_size {
+            return Colors[&Elem::User].paint(user_name).to_string();
+        }
+
+        let mut content = String::with_capacity(max_user_size);
+
+        content = content + user_name;
+
+        for _ in 0..(max_user_size - user_name.len()) {
+            content.push(' ');
+        }
+
+        content
+    }
+
+    pub fn format_group(&self, group_name: &String, max_group_size: usize) -> String {
+        if group_name.len() == max_group_size {
+            return Colors[&Elem::Group].paint(group_name).to_string();
+        }
+
+        let mut content = String::with_capacity(max_group_size);
+        content = content + group_name;
+
+        for _ in 0..(max_group_size - group_name.len()) {
+            content.push(' ');
+        }
+
+        content
+    }
+
+    pub fn format_size(
+        &self,
+        meta: &Meta,
+        max_value_length: usize,
+        max_unit_size: usize,
+    ) -> String {
+        let mut content = String::with_capacity(max_value_length + max_unit_size + 1);
+
+        for _ in 0..(max_value_length - meta.size_value.len()) {
+            content.push(' ');
+        }
+
+        content = content + meta.size_value.as_str();
+        content.push(' ');
+        content = content + meta.size_unit.as_str();
+
+        for _ in 0..(max_unit_size - meta.size_unit.len()) {
+            content.push(' ');
+        }
+
+        if meta.metadata.len() < 10 * 1044 * 1024 {
+            return Colors[&Elem::FileSmall].paint(content).to_string();
+        } else if meta.metadata.len() < 100 * 1044 * 1024 {
+            return Colors[&Elem::FileMedium].paint(content).to_string();
+        } else {
+            return Colors[&Elem::FileLarge].paint(content).to_string();
+        }
     }
 }
