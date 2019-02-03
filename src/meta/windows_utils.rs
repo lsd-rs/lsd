51a6257021231b44083a536bd91de1cfd67583e4
Daniel <dulaney.daniel@gmail.com>
2019-02-02 23:43:23 -0500

    Added utilities for working win Windows API calls

--- src/meta/windows_utils.rs
+++ src/meta/windows_utils.rs
@@ -1,7 +1,75 @@
 use std::io;
 use std::path::PathBuf;
+use std::ffi::{OsStr, OsString};
+use std::os::windows::ffi::{OsStringExt, OsStrExt};
 use super::{Owner, Permissions};
 
 pub fn get_file_data(path: &PathBuf) -> Result<(Owner, Permissions), io::Error> {
     unimplemented!()
+}
+
+/// Create an `OsString` from a NUL-terminated buffer
+///
+/// Decodes the WTF-16 encoded buffer until it hits a NUL (code point 0).
+/// Everything after and including that code point is not included.
+fn os_from_buf(buf: &[u16]) -> OsString {
+    OsString::from_wide(
+        &buf.iter()
+            .cloned()
+            .take_while(|&n| n != 0)
+            .collect::<Vec<u16>>(),
+    )
+}
+
+/// Create a WTF-16-encoded NUL-terminated buffer from an `OsStr`.
+///
+/// Decodes the `OsStr`, then appends a NUL.
+fn buf_from_os(os: &OsStr) -> Vec<u16> {
+    let mut buf: Vec<u16> = os.encode_wide().collect();
+    buf.push(0);
+    buf
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn basic_wtf16_behavior() {
+        let basic_os = OsString::from("TeSt");
+        let basic_buf = vec![0x54, 0x65, 0x53, 0x74, 0x00];
+        let basic_buf_nuls = vec![0x54, 0x65, 0x53, 0x74, 0x00, 0x00, 0x00, 0x00];
+
+        assert_eq!(os_from_buf(&basic_buf), basic_os);
+        assert_eq!(buf_from_os(&basic_os), basic_buf);
+        assert_eq!(os_from_buf(&basic_buf_nuls), basic_os);
+
+        let unicode_os = OsString::from("💩");
+        let unicode_buf = vec![0xd83d, 0xdca9, 0x0];
+        let unicode_buf_nuls = vec![0xd83d, 0xdca9, 0x0, 0x0, 0x0, 0x0, 0x0];
+
+        assert_eq!(os_from_buf(&unicode_buf), unicode_os);
+        assert_eq!(buf_from_os(&unicode_os), unicode_buf);
+        assert_eq!(os_from_buf(&unicode_buf_nuls), unicode_os);
+    }
+
+    #[test]
+    fn every_wtf16_codepair_roundtrip() {
+        for lsb in 0..256u16 {
+            let mut vec: Vec<u16> = Vec::with_capacity(257);
+
+            for msb in 0..=256u16 {
+                let val = msb << 8 | lsb;
+
+                if val != 0 { vec.push(val) }
+            }
+
+            vec.push(0);
+
+            let os = os_from_buf(&vec);
+            let new_vec = buf_from_os(&os);
+
+            assert_eq!(&vec, &new_vec);
+        }
+    }
 }
