78726b3a9e0320322a45b3f1dab7998f88eed86e
Sophie Tauchert <999eagle@999eagle.moe>
2021-01-13 14:37:02 +0100

    Add test cases for iso and locale formatting

--- src/meta/date.rs
+++ src/meta/date.rs
@@ -62,7 +62,7 @@ mod test {
     use crate::color::{Colors, Theme};
     use crate::flags::{DateFlag, Flags};
     use ansi_term::Colour;
-    use chrono::{DateTime, Local};
+    use chrono::{DateTime, Duration, Local};
     use std::io;
     use std::path::Path;
     use std::process::{Command, ExitStatus};
@@ -218,6 +218,106 @@ mod test {
         flags.date = DateFlag::Relative;
 
         assert_eq!(Colour::Fixed(40).paint("now"), date.render(&colors, &flags));
+
+        fs::remove_file(file_path).unwrap();
+    }
+
+    #[test]
+    fn test_iso_format_now() {
+        let mut file_path = env::temp_dir();
+        file_path.push("test_iso_format_now.tmp");
+
+        let creation_date = Local::now();
+        let success = cross_platform_touch(&file_path, &creation_date)
+            .unwrap()
+            .success();
+        assert_eq!(true, success, "failed to exec touch");
+
+        let colors = Colors::new(Theme::Default);
+        let date = Date::from(&file_path.metadata().unwrap());
+
+        let mut flags = Flags::default();
+        flags.date = DateFlag::ISO;
+
+        assert_eq!(
+            Colour::Fixed(40).paint(creation_date.format("%m-%d %R").to_string()),
+            date.render(&colors, &flags)
+        );
+
+        fs::remove_file(file_path).unwrap();
+    }
+
+    #[test]
+    fn test_iso_format_year_old() {
+        let mut file_path = env::temp_dir();
+        file_path.push("test_iso_format_year_old.tmp");
+
+        let creation_date = Local::now() - Duration::days(400);
+        let success = cross_platform_touch(&file_path, &creation_date)
+            .unwrap()
+            .success();
+        assert_eq!(true, success, "failed to exec touch");
+
+        let colors = Colors::new(Theme::Default);
+        let date = Date::from(&file_path.metadata().unwrap());
+
+        let mut flags = Flags::default();
+        flags.date = DateFlag::ISO;
+
+        assert_eq!(
+            Colour::Fixed(36).paint(creation_date.format("%F").to_string()),
+            date.render(&colors, &flags)
+        );
+
+        fs::remove_file(file_path).unwrap();
+    }
+
+    #[test]
+    fn test_locale_format_now() {
+        let mut file_path = env::temp_dir();
+        file_path.push("test_locale_format_now.tmp");
+
+        let creation_date = Local::now();
+        let success = cross_platform_touch(&file_path, &creation_date)
+            .unwrap()
+            .success();
+        assert_eq!(true, success, "failed to exec touch");
+
+        let colors = Colors::new(Theme::Default);
+        let date = Date::from(&file_path.metadata().unwrap());
+
+        let mut flags = Flags::default();
+        flags.date = DateFlag::Locale;
+
+        assert_eq!(
+            Colour::Fixed(40).paint(creation_date.format("%b %d %R").to_string()),
+            date.render(&colors, &flags)
+        );
+
+        fs::remove_file(file_path).unwrap();
+    }
+
+    #[test]
+    fn test_locale_format_year_old() {
+        let mut file_path = env::temp_dir();
+        file_path.push("test_locale_format_year_old.tmp");
+
+        let creation_date = Local::now() - Duration::days(400);
+        let success = cross_platform_touch(&file_path, &creation_date)
+            .unwrap()
+            .success();
+        assert_eq!(true, success, "failed to exec touch");
+
+        let colors = Colors::new(Theme::Default);
+        let date = Date::from(&file_path.metadata().unwrap());
+
+        let mut flags = Flags::default();
+        flags.date = DateFlag::Locale;
+
+        assert_eq!(
+            Colour::Fixed(36).paint(creation_date.format("%b %d  %Y").to_string()),
+            date.render(&colors, &flags)
+        );
 
         fs::remove_file(file_path).unwrap();
     }
