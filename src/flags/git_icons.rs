dcfcec9243406fa52e86f7681964d46a66afc940
Pascal HAVÉ <hpwxf@haveneer.com>
2021-03-22 23:09:36 +0100

    Add non redundancy test on git icons

--- src/flags/git_icons.rs
+++ src/flags/git_icons.rs
@@ -34,23 +34,70 @@ impl GitIcons {
         .to_string()
     }
 
+    // On each unicode icon, add its value in a comment like "\ue5fb" (cf https://www.nerdfonts.com/cheat-sheet)
+    // and then run the command below in vim:
+    // s#\\u[0-9a-f]\{4}#\=eval('"'.submatch(0).'"')#
     fn get_icon(&self, status: &GitStatus) -> String {
         match status {
             GitStatus::Default => "_",
-            GitStatus::Unmodified => "_", // "\u{f00c}"
-            GitStatus::NewInIndex => "\u{f067}",
+            GitStatus::Unmodified => "_",
+            GitStatus::NewInIndex => "\u{f067}", // ""
             GitStatus::NewInWorkdir => "?",
-            GitStatus::Deleted => "\u{f014}", // or f068
-            GitStatus::Modified => "\u{f8ea}",
-            GitStatus::Renamed => "\u{f8ea}",
+            GitStatus::Deleted => "\u{f014}",  // ""
+            GitStatus::Modified => "\u{f8ea}", // ""
+            GitStatus::Renamed => "\u{f02b}",  // ""
             GitStatus::Ignored => "!",
-            GitStatus::Typechange => "\u{f0ec}",
-            GitStatus::Conflicted => "\u{f071}",
+            GitStatus::Typechange => "\u{f0ec}", // ""
+            GitStatus::Conflicted => "\u{f071}", // ""
         }
         .to_string()
     }
 
     fn get_unicode(&self, status: &GitStatus) -> String {
         self.get_text(status)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::Theme;
+    use crate::flags::git_icons::GitIcons;
+    use crate::git::GitStatus;
+    use std::collections::HashMap;
+    use strum::IntoEnumIterator;
+
+    fn test_non_duplicated(icons: &GitIcons) {
+        assert_eq!(
+            icons.get(&GitStatus::Default),
+            icons.get(&GitStatus::Unmodified)
+        );
+        let mut m = HashMap::new();
+        for status in GitStatus::iter() {
+            if status == GitStatus::Default {
+                continue;
+            }
+            assert_eq!(m.insert(icons.get(&status), status), None);
+        }
+    }
+
+    #[cfg(feature = "git")]
+    #[test]
+    fn test_non_duplicated_noicon() {
+        let icons = GitIcons::new(Theme::NoIcon);
+        test_non_duplicated(&icons);
+    }
+
+    #[cfg(feature = "git")]
+    #[test]
+    fn test_non_duplicated_unicode() {
+        let icons = GitIcons::new(Theme::Unicode);
+        test_non_duplicated(&icons);
+    }
+
+    #[cfg(feature = "git")]
+    #[test]
+    fn test_non_duplicated_fancy() {
+        let icons = GitIcons::new(Theme::Fancy);
+        test_non_duplicated(&icons);
     }
 }
