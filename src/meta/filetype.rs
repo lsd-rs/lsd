21704fefb09167a0a8669f334790793f7e10f3d0
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-04-22 00:07:42 -0400

    change order to put msi back in the original spot

--- src/meta/filetype.rs
+++ src/meta/filetype.rs
@@ -16,7 +16,7 @@ pub enum FileType {
 
 impl FileType {
     #[cfg(windows)]
-    const EXECUTABLE_EXTENSIONS: &[&'static str] = &["exe", "bat", "ps1", "cmd", "com", "msi" ];
+    const EXECUTABLE_EXTENSIONS: &[&'static str] = &["exe", "msi", "bat", "ps1", "cmd", "com" ];
 
     #[cfg(unix)]
     pub fn new(
