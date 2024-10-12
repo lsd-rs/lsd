168acdf3abc7d3d79d3326a29b0fd91be128da6d
Jorik <vanveenjorik@protonmail.com>
2024-10-12 17:57:45 +0200

    Add flake.nix and .envrc

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          cargo
+          rustc
+          rust-analyzer
+        ];
+      };
+    }
+  );
+}
