7dbaf51c943b7d5dbccd2b04727e6bd43b9d6b98
RagnarokMew <101945097+RagnarokMew@users.noreply.github.com>
2025-03-23 17:01:45 +0200

    Format code

--- src/meta/mod.rs
+++ src/meta/mod.rs
@@ -372,7 +372,10 @@ mod tests {
     use crate::flags::PermissionFlag;
 
     use super::Meta;
-    use std::{fs::File, io::{BufWriter, Write}};
+    use std::{
+        fs::File,
+        io::{BufWriter, Write},
+    };
     use tempfile::tempdir;
 
     #[cfg(unix)]
@@ -454,12 +457,15 @@ mod tests {
 
         let mut buff_writer = BufWriter::new(file);
 
-        let buffer = vec![0u8;100];
+        let buffer = vec![0u8; 100];
 
-        buff_writer.write_all(&buffer).expect("failed to write bytes to file");
-        buff_writer.flush().expect("failed to write all bytes to file");
+        buff_writer
+            .write_all(&buffer)
+            .expect("failed to write bytes to file");
+        buff_writer
+            .flush()
+            .expect("failed to write all bytes to file");
 
         assert!(Meta::calculate_total_file_size(path_file.as_path()) == 100);
     }
-
 }
